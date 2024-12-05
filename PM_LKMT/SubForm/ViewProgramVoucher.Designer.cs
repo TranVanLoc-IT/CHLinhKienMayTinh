90de2c3cc77bd5d871c1e3dc1e33c65c88159949
hienprohienvip <hienprohienvip@gmail.com>
2024-12-05 14:37:41 +0700

    feature_hien/vourcher

--- PM_LKMT/SubForm/ViewProgramVoucher.Designer.cs
+++ PM_LKMT/SubForm/ViewProgramVoucher.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
             label1 = new Label();
             dgvProgramVour = new DataGridView();
             label3 = new Label();
@@ -41,229 +41,281 @@
             label6 = new Label();
             label7 = new Label();
             txtConditionApply = new TextBox();
-            btnAdd = new Button();
-            btnDelete = new Button();
-            btnUpdate = new Button();
             txtProgramId = new TextBox();
             label2 = new Label();
             label8 = new Label();
             cboDaXoa = new ComboBox();
+            btnAdd = new Button();
             btnKhoiPhuc = new Button();
+            btnDelete = new Button();
+            btnUpdate = new Button();
             ((System.ComponentModel.ISupportInitialize)dgvProgramVour).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
+            label1.BackColor = Color.White;
             label1.Dock = DockStyle.Top;
-            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
             label1.Location = new Point(0, 0);
             label1.Name = "label1";
-            label1.Size = new Size(1142, 53);
+            label1.Size = new Size(1783, 74);
             label1.TabIndex = 0;
             label1.Text = "Chương trình khuyến mãi";
             label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // dgvProgramVour
             // 
+            dgvProgramVour.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dgvProgramVour.BackgroundColor = Color.White;
             dgvProgramVour.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = SystemColors.Window;
-            dataGridViewCellStyle1.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            dataGridViewCellStyle1.ForeColor = SystemColors.ControlText;
-            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.False;
-            dgvProgramVour.DefaultCellStyle = dataGridViewCellStyle1;
-            dgvProgramVour.Location = new Point(12, 56);
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = SystemColors.Window;
+            dataGridViewCellStyle3.Font = new Font("Segoe UI", 14F);
+            dataGridViewCellStyle3.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
+            dgvProgramVour.DefaultCellStyle = dataGridViewCellStyle3;
+            dgvProgramVour.Location = new Point(0, 77);
             dgvProgramVour.Name = "dgvProgramVour";
             dgvProgramVour.RowHeadersWidth = 62;
-            dgvProgramVour.RowTemplate.Height = 33;
-            dgvProgramVour.Size = new Size(1121, 310);
+            dgvProgramVour.Size = new Size(1783, 262);
             dgvProgramVour.TabIndex = 1;
             // 
             // label3
             // 
+            label3.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(613, 407);
+            label3.Font = new Font("Segoe UI", 14F);
+            label3.Location = new Point(971, 365);
             label3.Name = "label3";
-            label3.Size = new Size(148, 30);
+            label3.Size = new Size(189, 38);
             label3.TabIndex = 4;
             label3.Text = "Ngày bắt đầu:";
             // 
             // txtDateStart
             // 
-            txtDateStart.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            txtDateStart.Location = new Point(807, 404);
+            txtDateStart.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            txtDateStart.Font = new Font("Segoe UI", 14F);
+            txtDateStart.Location = new Point(1183, 362);
             txtDateStart.Name = "txtDateStart";
             txtDateStart.PlaceholderText = "dd/MM/yyyy";
-            txtDateStart.Size = new Size(246, 37);
+            txtDateStart.Size = new Size(595, 45);
             txtDateStart.TabIndex = 5;
             // 
             // txtPercentValue
             // 
-            txtPercentValue.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            txtPercentValue.Location = new Point(217, 450);
+            txtPercentValue.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            txtPercentValue.Font = new Font("Segoe UI", 14F);
+            txtPercentValue.Location = new Point(268, 421);
             txtPercentValue.Name = "txtPercentValue";
-            txtPercentValue.Size = new Size(246, 37);
+            txtPercentValue.Size = new Size(625, 45);
             txtPercentValue.TabIndex = 7;
             // 
             // label4
             // 
+            label4.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(12, 447);
+            label4.Font = new Font("Segoe UI", 14F);
+            label4.Location = new Point(15, 418);
             label4.Name = "label4";
-            label4.Size = new Size(175, 30);
+            label4.Size = new Size(226, 38);
             label4.TabIndex = 6;
             label4.Text = "Giá trị phần trăm";
             // 
             // txtDateEnd
             // 
-            txtDateEnd.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            txtDateEnd.Location = new Point(807, 446);
+            txtDateEnd.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            txtDateEnd.Font = new Font("Segoe UI", 14F);
+            txtDateEnd.Location = new Point(1183, 414);
             txtDateEnd.Name = "txtDateEnd";
             txtDateEnd.PlaceholderText = "dd/MM/yyyy";
-            txtDateEnd.Size = new Size(246, 37);
+            txtDateEnd.Size = new Size(595, 45);
             txtDateEnd.TabIndex = 9;
             // 
             // label5
             // 
+            label5.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            label5.Location = new Point(613, 453);
+            label5.Font = new Font("Segoe UI", 14F);
+            label5.Location = new Point(971, 421);
             label5.Name = "label5";
-            label5.Size = new Size(147, 30);
+            label5.Size = new Size(190, 38);
             label5.TabIndex = 8;
             label5.Text = "Ngày kết thúc";
             // 
             // txtPercentInvoice
             // 
-            txtPercentInvoice.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            txtPercentInvoice.Location = new Point(807, 495);
+            txtPercentInvoice.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            txtPercentInvoice.Font = new Font("Segoe UI", 14F);
+            txtPercentInvoice.Location = new Point(1183, 469);
             txtPercentInvoice.Name = "txtPercentInvoice";
-            txtPercentInvoice.Size = new Size(246, 37);
+            txtPercentInvoice.Size = new Size(595, 45);
             txtPercentInvoice.TabIndex = 11;
             // 
             // label6
             // 
+            label6.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            label6.Location = new Point(613, 504);
+            label6.Font = new Font("Segoe UI", 14F);
+            label6.Location = new Point(971, 478);
             label6.Name = "label6";
-            label6.Size = new Size(156, 30);
+            label6.Size = new Size(202, 38);
             label6.TabIndex = 10;
             label6.Text = "Giá trị hóa đơn";
             // 
             // label7
             // 
+            label7.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             label7.AutoSize = true;
-            label7.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            label7.Location = new Point(9, 498);
+            label7.Font = new Font("Segoe UI", 14F);
+            label7.Location = new Point(12, 480);
             label7.Name = "label7";
-            label7.Size = new Size(189, 30);
+            label7.Size = new Size(245, 38);
             label7.TabIndex = 12;
             label7.Text = "Điều kiện áp dụng";
             // 
             // txtConditionApply
             // 
-            txtConditionApply.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            txtConditionApply.Location = new Point(217, 501);
+            txtConditionApply.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            txtConditionApply.Font = new Font("Segoe UI", 14F);
+            txtConditionApply.Location = new Point(268, 483);
             txtConditionApply.Multiline = true;
             txtConditionApply.Name = "txtConditionApply";
-            txtConditionApply.Size = new Size(246, 88);
+            txtConditionApply.Size = new Size(625, 88);
             txtConditionApply.TabIndex = 13;
-            // 
-            // btnAdd
-            // 
-            btnAdd.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            btnAdd.Location = new Point(301, 611);
-            btnAdd.Name = "btnAdd";
-            btnAdd.Size = new Size(119, 48);
-            btnAdd.TabIndex = 14;
-            btnAdd.Text = "Thêm mới";
-            btnAdd.UseVisualStyleBackColor = true;
-            // 
-            // btnDelete
-            // 
-            btnDelete.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            btnDelete.Location = new Point(449, 612);
-            btnDelete.Name = "btnDelete";
-            btnDelete.Size = new Size(120, 46);
-            btnDelete.TabIndex = 15;
-            btnDelete.Text = "Xóa";
-            btnDelete.UseVisualStyleBackColor = true;
-            // 
-            // btnUpdate
-            // 
-            btnUpdate.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            btnUpdate.Location = new Point(595, 611);
-            btnUpdate.Name = "btnUpdate";
-            btnUpdate.Size = new Size(122, 48);
-            btnUpdate.TabIndex = 16;
-            btnUpdate.Text = "Cập nhật";
-            btnUpdate.UseVisualStyleBackColor = true;
             // 
             // txtProgramId
             // 
-            txtProgramId.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            txtProgramId.Location = new Point(217, 407);
+            txtProgramId.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            txtProgramId.Font = new Font("Segoe UI", 14F);
+            txtProgramId.Location = new Point(268, 365);
             txtProgramId.Name = "txtProgramId";
-            txtProgramId.Size = new Size(246, 37);
+            txtProgramId.Size = new Size(625, 45);
             txtProgramId.TabIndex = 18;
             // 
             // label2
             // 
+            label2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(12, 407);
+            label2.Font = new Font("Segoe UI", 14F);
+            label2.Location = new Point(15, 365);
             label2.Name = "label2";
-            label2.Size = new Size(178, 30);
+            label2.Size = new Size(224, 38);
             label2.TabIndex = 17;
-            label2.Text = "Mã chương trình:";
+            label2.Text = "Mã chương trình";
             // 
             // label8
             // 
+            label8.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             label8.AutoSize = true;
-            label8.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            label8.Location = new Point(613, 550);
+            label8.Font = new Font("Segoe UI", 14F);
+            label8.Location = new Point(971, 534);
             label8.Name = "label8";
-            label8.Size = new Size(84, 30);
+            label8.Size = new Size(108, 38);
             label8.TabIndex = 19;
             label8.Text = "Đã xóa:";
             // 
             // cboDaXoa
             // 
-            cboDaXoa.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            cboDaXoa.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            cboDaXoa.Font = new Font("Segoe UI", 14F);
             cboDaXoa.FormattingEnabled = true;
-            cboDaXoa.Location = new Point(807, 542);
+            cboDaXoa.Location = new Point(1183, 526);
             cboDaXoa.Name = "cboDaXoa";
-            cboDaXoa.Size = new Size(246, 38);
+            cboDaXoa.Size = new Size(595, 46);
             cboDaXoa.TabIndex = 20;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Anchor = AnchorStyles.Bottom;
+            btnAdd.BackColor = Color.DarkGreen;
+            btnAdd.BackgroundImage = Resource.add;
+            btnAdd.BackgroundImageLayout = ImageLayout.None;
+            btnAdd.FlatAppearance.MouseOverBackColor = Color.Green;
+            btnAdd.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnAdd.ForeColor = SystemColors.ButtonFace;
+            btnAdd.Location = new Point(516, 597);
+            btnAdd.Margin = new Padding(4);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(149, 74);
+            btnAdd.TabIndex = 25;
+            btnAdd.Text = "Thêm";
+            btnAdd.TextAlign = ContentAlignment.MiddleRight;
+            btnAdd.UseVisualStyleBackColor = false;
             // 
             // btnKhoiPhuc
             // 
-            btnKhoiPhuc.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
-            btnKhoiPhuc.Location = new Point(743, 611);
+            btnKhoiPhuc.Anchor = AnchorStyles.Bottom;
+            btnKhoiPhuc.BackColor = Color.Green;
+            btnKhoiPhuc.BackgroundImage = Resource.update2;
+            btnKhoiPhuc.BackgroundImageLayout = ImageLayout.None;
+            btnKhoiPhuc.FlatAppearance.MouseOverBackColor = Color.Lime;
+            btnKhoiPhuc.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnKhoiPhuc.ForeColor = SystemColors.ButtonFace;
+            btnKhoiPhuc.ImageAlign = ContentAlignment.MiddleLeft;
+            btnKhoiPhuc.Location = new Point(1150, 597);
+            btnKhoiPhuc.Margin = new Padding(4);
             btnKhoiPhuc.Name = "btnKhoiPhuc";
-            btnKhoiPhuc.Size = new Size(123, 48);
-            btnKhoiPhuc.TabIndex = 21;
-            btnKhoiPhuc.Text = "Khôi phục";
-            btnKhoiPhuc.UseVisualStyleBackColor = true;
+            btnKhoiPhuc.Size = new Size(149, 74);
+            btnKhoiPhuc.TabIndex = 26;
+            btnKhoiPhuc.Text = "Restore";
+            btnKhoiPhuc.TextAlign = ContentAlignment.MiddleRight;
+            btnKhoiPhuc.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Anchor = AnchorStyles.Bottom;
+            btnDelete.BackColor = Color.DarkRed;
+            btnDelete.BackgroundImage = Resource.delete;
+            btnDelete.BackgroundImageLayout = ImageLayout.None;
+            btnDelete.FlatAppearance.MouseOverBackColor = Color.Red;
+            btnDelete.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnDelete.ForeColor = SystemColors.ButtonFace;
+            btnDelete.Location = new Point(734, 597);
+            btnDelete.Margin = new Padding(4);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(149, 74);
+            btnDelete.TabIndex = 27;
+            btnDelete.Text = "Xóa";
+            btnDelete.TextAlign = ContentAlignment.MiddleRight;
+            btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Anchor = AnchorStyles.Bottom;
+            btnUpdate.BackColor = Color.DarkCyan;
+            btnUpdate.BackgroundImage = Resource.update2;
+            btnUpdate.BackgroundImageLayout = ImageLayout.None;
+            btnUpdate.FlatAppearance.MouseOverBackColor = Color.Lime;
+            btnUpdate.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnUpdate.ForeColor = SystemColors.ButtonFace;
+            btnUpdate.ImageAlign = ContentAlignment.MiddleLeft;
+            btnUpdate.Location = new Point(945, 598);
+            btnUpdate.Margin = new Padding(4);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(149, 74);
+            btnUpdate.TabIndex = 28;
+            btnUpdate.Text = "Sửa";
+            btnUpdate.TextAlign = ContentAlignment.MiddleRight;
+            btnUpdate.UseVisualStyleBackColor = false;
             // 
             // ViewProgramVoucher
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1142, 679);
+            BackColor = Color.White;
+            ClientSize = new Size(1783, 748);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnDelete);
             Controls.Add(btnKhoiPhuc);
+            Controls.Add(btnAdd);
             Controls.Add(cboDaXoa);
             Controls.Add(label8);
             Controls.Add(txtProgramId);
             Controls.Add(label2);
-            Controls.Add(btnUpdate);
-            Controls.Add(btnDelete);
-            Controls.Add(btnAdd);
             Controls.Add(txtConditionApply);
             Controls.Add(label7);
             Controls.Add(txtPercentInvoice);
@@ -297,13 +349,13 @@
         private Label label6;
         private Label label7;
         private TextBox txtConditionApply;
-        private Button btnAdd;
-        private Button btnDelete;
-        private Button btnUpdate;
         private TextBox txtProgramId;
         private Label label2;
         private Label label8;
         private ComboBox cboDaXoa;
-        private Button btnKhoiPhuc;
+        public Button btnAdd;
+        public Button btnKhoiPhuc;
+        public Button btnDelete;
+        public Button btnUpdate;
     }
 }
---
+++ PM_LKMT/SubForm/ViewVourcher.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace PM_LKMT.SubForm
+{
+    partial class ViewVourcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            dvgVourcher = new DataGridView();
+            label2 = new Label();
+            txtMaKM = new TextBox();
+            txtMaCT = new TextBox();
+            label3 = new Label();
+            txtSoLuong = new TextBox();
+            label4 = new Label();
+            btnTaoMoi = new Button();
+            btnXoa = new Button();
+            btnCapNhat = new Button();
+            ((System.ComponentModel.ISupportInitialize)dvgVourcher).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Dock = DockStyle.Top;
+            label1.Font = new Font("Times New Roman", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(926, 91);
+            label1.TabIndex = 0;
+            label1.Text = "KHUYẾN MÃI";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // dvgVourcher
+            // 
+            dvgVourcher.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dvgVourcher.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dvgVourcher.Location = new Point(0, 74);
+            dvgVourcher.Name = "dvgVourcher";
+            dvgVourcher.RowHeadersWidth = 62;
+            dvgVourcher.RowTemplate.Height = 33;
+            dvgVourcher.Size = new Size(926, 338);
+            dvgVourcher.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(12, 465);
+            label2.Name = "label2";
+            label2.Size = new Size(148, 25);
+            label2.TabIndex = 2;
+            label2.Text = "Mã khuyến mãi";
+            // 
+            // txtMaKM
+            // 
+            txtMaKM.Font = new Font("Times New Roman", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            txtMaKM.Location = new Point(188, 462);
+            txtMaKM.Name = "txtMaKM";
+            txtMaKM.Size = new Size(239, 33);
+            txtMaKM.TabIndex = 3;
+            // 
+            // txtMaCT
+            // 
+            txtMaCT.Enabled = false;
+            txtMaCT.Font = new Font("Times New Roman", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            txtMaCT.Location = new Point(188, 501);
+            txtMaCT.Name = "txtMaCT";
+            txtMaCT.Size = new Size(239, 33);
+            txtMaCT.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(12, 504);
+            label3.Name = "label3";
+            label3.Size = new Size(156, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Mã chương trình";
+            // 
+            // txtSoLuong
+            // 
+            txtSoLuong.Font = new Font("Times New Roman", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            txtSoLuong.Location = new Point(596, 457);
+            txtSoLuong.Name = "txtSoLuong";
+            txtSoLuong.Size = new Size(303, 33);
+            txtSoLuong.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(469, 465);
+            label4.Name = "label4";
+            label4.Size = new Size(91, 25);
+            label4.TabIndex = 6;
+            label4.Text = "Số lượng";
+            // 
+            // btnTaoMoi
+            // 
+            btnTaoMoi.Font = new Font("Times New Roman", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            btnTaoMoi.Location = new Point(471, 504);
+            btnTaoMoi.Name = "btnTaoMoi";
+            btnTaoMoi.Size = new Size(122, 46);
+            btnTaoMoi.TabIndex = 8;
+            btnTaoMoi.Text = "Tạo mới";
+            btnTaoMoi.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            btnXoa.Font = new Font("Times New Roman", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            btnXoa.Location = new Point(625, 504);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(122, 46);
+            btnXoa.TabIndex = 9;
+            btnXoa.Text = "Xóa";
+            btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnCapNhat
+            // 
+            btnCapNhat.Font = new Font("Times New Roman", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCapNhat.Location = new Point(777, 504);
+            btnCapNhat.Name = "btnCapNhat";
+            btnCapNhat.Size = new Size(122, 46);
+            btnCapNhat.TabIndex = 10;
+            btnCapNhat.Text = "Cập nhật";
+            btnCapNhat.UseVisualStyleBackColor = true;
+            // 
+            // ViewVourcher
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(926, 597);
+            Controls.Add(btnCapNhat);
+            Controls.Add(btnXoa);
+            Controls.Add(btnTaoMoi);
+            Controls.Add(txtSoLuong);
+            Controls.Add(label4);
+            Controls.Add(txtMaCT);
+            Controls.Add(label3);
+            Controls.Add(txtMaKM);
+            Controls.Add(label2);
+            Controls.Add(dvgVourcher);
+            Controls.Add(label1);
+            Name = "ViewVourcher";
+            Text = "ViewVourcher";
+            ((System.ComponentModel.ISupportInitialize)dvgVourcher).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private DataGridView dvgVourcher;
+        private Label label2;
+        private TextBox txtMaKM;
+        private TextBox txtMaCT;
+        private Label label3;
+        private TextBox txtSoLuong;
+        private Label label4;
+        private Button btnTaoMoi;
+        private Button btnXoa;
+        private Button btnCapNhat;
+    }
+}
